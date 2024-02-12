e5f7e5cb2586f287019a3ac68489e45955550a02
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2024-02-12 09:48:21 +0200

    add program.cs

---
+++ ConsoleApp10/Engineer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Engineer:Employee
+    {
+        private int n;
+
+        public Engineer() { }
+
+        public Engineer(string name, string surname, DateTime birth, int p, int n) : base(name, surname, birth, p)
+        {
+            this.n = n;
+        }
+
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Кол-во разработанных проектов: {n}");
+        }
+
+        public new double Income() 
+        {
+           
+        }
+    }
+}
---
+++ ConsoleApp10/Manager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Manager:Employee
+    {
+        private int sales;
+
+
+        public Manager() { }
+        public Manager(string name, string surname, DateTime birth, int p,int sales) :base(name, surname,birth,p)
+        {
+            this.sales = sales;
+        }
+
+        public int Sales
+        { 
+            get { return sales; }
+            set { sales = value; }
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Объем продаж {sales}");
+        }
+        public override double Income(double k) 
+        {
+            double H = k * P;
+            if (sales > H )
+            {
+                return H * 0.01;
+            }
+            else
+            {
+                return base.Income(k);
+            }
+            return k += H;
+        }
+
+
+    }
+}
